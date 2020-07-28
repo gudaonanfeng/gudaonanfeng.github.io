a2cabe263ab784be4216d75360215a2f620c428b
gudaonanfeng <2399031141@qq.com>
2020-07-29 01:46:12 +0800

    Site updated: 2020-07-29 01:46:11

--- js/main.js
+++ js/main.js
@@ -790,3 +790,9 @@ $(function () {
     }
   })
 })
+
+// 去掉首页banner
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
